656da5da7c8d83b066befeb2fc352e0bd6d957a7
May_Phyu_Phyu_Aung <mayphyuaung2018@gmail.com>
2021-11-22 07:59:25 -0800

    init commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into employee(employee_id,first_name,last_name,email,phone_number,hire_data,salary)
+values(1,'John', 'Doe', 'john@gmail.com','555-555-678','2020-06-05',70000)
+        (2,'Willion','James', 'willion@gmail.com','456-345-344', '2020-03-01',80000)
+        (3,'Mary', 'Brown','mary@gmail.com','212-563-234','2020-01-12',90000)
